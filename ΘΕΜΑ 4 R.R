0ad6324226fa44d41cb088f0115de270242ec3a5
filipakos95 <115537388+filipakos95@users.noreply.github.com>
2022-11-02 17:29:38 +0200

    Update ΘΕΜΑ 4 R.R

--- ΘΕΜΑ 4 R.R
+++ ΘΕΜΑ 4 R.R
@@ -9,7 +9,7 @@ euclidean <- function(x, y) sqrt(sum((x - y)^2))
 #orismos dianysmatwn
 
 x1 <- c(1,2,3,4,5,6) 
-y1<-c(1,2,3,4,5,6)
+y1<-  c(1,2,3,4,5,6)
 
 x2 <- c(-0.5, 1, 7.3, 7, 9.4, -8.2, 9, -6, -6.3) 
 y2 <- c(0.5, -1, -7.3, -7, -9.4, 8.2, -9, 6, 6.3)
@@ -38,9 +38,11 @@ s<- c(3,55000,22,5)
 t<-c(4,27000,13,11)
 u<- c(5,58000,21,13) 
 
+rown <- c("p","r","s","t","u")
+coln <- c("ΑΑ","ΔΙΑΡΚΕΙΑ","ΠΛΗΘΟΣ","SMS","MB")
 #ftiaxnw ton pinaka dianysmatwm
-k <- matrix(c(p,r,s,t,u), nrow = 5, byrow = TRUE, 
-            dimnames = list(rown, coln))
+k <- matrix(c(p,r,s,t,u), nrow = 5, byrow = TRUE, dimnames = list(rown, coln))
+print(k)
 
 #ypologizw tis apostaseis
 euclidean(p,u)
@@ -53,6 +55,10 @@ apostasi1<-euclidean(p,u)
 apostasi2<-euclidean(r,u)
 apostasi3<-euclidean(s,u)
 apostasi4<-euclidean(t,u)
+print(apostasi1)
+print(apostasi2)
+print(apostasi3)
+print(apostasi4)
 
 #briskw thn mikroterh apostash h opoia tha tairiazei sto profile toy xrhsth 5
 min(apostasi1,apostasi2,apostasi3,apostasi4)
